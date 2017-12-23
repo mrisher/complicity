5d43f3746c032e81236f3eb7236b3b4e32415600
Mark Risher <mrisher@gmail.com>
2017-12-22 16:06:31 -0800

    basic, responsive site

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import logging, base64, os
 from flask import Flask, render_template, request
+from flask_talisman import Talisman, GOOGLE_CSP_POLICY
 app = Flask(__name__)
+
+# Set up Talisman for app security and CSP
+
+csp = {
+    # Fonts from fonts.google.com
+    'font-src': '\'self\' themes.googleusercontent.com *.gstatic.com',
+    # <iframe> based embedding for Maps and Youtube.
+    'frame-src': '\'self\' www.google.com www.youtube.com',
+    # Assorted Google-hosted Libraries/APIs.
+    'script-src': '\'self\' ajax.googleapis.com *.googleanalytics.com '
+                  '*.google-analytics.com',
+    # Used by generated code from http://www.google.com/fonts
+    'style-src': '\'self\' ajax.googleapis.com fonts.googleapis.com '
+                 '*.gstatic.com ',
+    'default-src': '\'self\' *.gstatic.com',
+}
+Talisman(app, content_security_policy=GOOGLE_CSP_POLICY)
+
 
 @app.route('/form')
 def form():
