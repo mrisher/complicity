b9a668596a359c77f7d1ee7944d2f88fea78175e
Mark Risher <mrisher@gmail.com>
2017-12-28 22:24:20 -0800

    Vue skeleton working
    Runs in node with `npm run dev` and with Flask if `npm run build`
    followed by running flask itself (dev_appserver.py app.yaml)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging, base64, os, re
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, current_app
 from flask_talisman import Talisman, GOOGLE_CSP_POLICY
-app = Flask(__name__)
+app = Flask(__name__, static_folder='vue-project/dist')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # Set up Talisman for app security and CSP
@@ -44,3 +44,8 @@ def run_prototype(start = 0):
     #logging.debug("script_src = '{}'".format(script_src))
     #talisman.content_security_policy['script-src'] = script_src 
     return render_template('scene.html', start=start, nonce=nonce)
+
+@app.route('/vue')
+def run_vue():
+    return current_app.send_static_file('index.html')
+
